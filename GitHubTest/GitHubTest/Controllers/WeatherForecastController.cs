9799b9a8a815b57e264b2622c72b72e9cbd657e4
Jason <diasjason7@gmail.com>
2019-11-28 13:59:02 -0800

    bad checkin

--- GitHubTest/GitHubTest/Controllers/WeatherForecastController.cs
+++ GitHubTest/GitHubTest/Controllers/WeatherForecastController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace GitHubTest.Controllers
-{ //check into master test
+{ //check into master test with protection
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
