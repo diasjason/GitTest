591970491813099e6e8c69a70d2749bc9fa905a9
Jason <diasjason7@gmail.com>
2019-11-28 16:15:08 -0800

    Testing ci checking

--- GitHubTest/GitHubTest/Controllers/WeatherForecastController.cs
+++ GitHubTest/GitHubTest/Controllers/WeatherForecastController.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Logging;
 
 namespace GitHubTest.Controllers
 { //check into master test with protection
+    //what happens how i test it
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
